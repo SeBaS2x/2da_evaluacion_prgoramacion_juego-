60baa89f210fe7291f7f0878d0b95be3ca217bb9
Juan <juanpablovila73@gmail.com>
2025-07-06 14:43:28 -0300

    hice el menu interactivo del juego

--- funciones.py
+++ funciones.py
@@ -1,11 +1,14 @@
 import sys
+
 sys.stdout.reconfigure(encoding="utf-8")
-def crear_matriz (cantidad_f:int, cantidad_c:int, valor)->list:
+
+
+def crear_matriz(cantidad_f: int, cantidad_c: int, valor) -> list:
     """Crea matriz vacia, pasar cantidad de filas, columnas y el valor
 
     Args:
         cantidad_f (_type_): Cantidad de filas
-        cantidad_c (_type_): Cantidad de columnas 
+        cantidad_c (_type_): Cantidad de columnas
         valor (_type_): valor de inicializacion
 
     Returns:
@@ -13,12 +16,13 @@ def crear_matriz (cantidad_f:int, cantidad_c:int, valor)->list:
     """
     matriz = []
     for i in range(cantidad_f):
-        fila = [valor] *  cantidad_c
+        fila = [valor] * cantidad_c
         matriz += [fila]
     return matriz
 
+
 def llamar_csv():
-    """LLama el archivo .csv y lo carga en una matriz vacia 
+    """LLama el archivo .csv y lo carga en una matriz vacia
 
     Returns:
         _type_: Devuelve Columnas: primera fila con el valor de las columnas.
@@ -26,14 +30,14 @@ def llamar_csv():
     """
     with open("preguntas_superheroes.csv", "r", encoding="utf-8") as archivo:
         linea = archivo.readline()
-        columnas =[]
+        columnas = []
         valor = ""
         for c in linea:
             if c == "\n" or c == ",":
                 columnas += [valor]
                 valor = ""
             else:
-                valor +=c
+                valor += c
         filas = archivo.readlines()
         num_filas = len(filas)
         num_columnas = len(columnas)
@@ -49,23 +53,25 @@ def llamar_csv():
                     else:
                         matriz[i][col] = valor
                     valor = ""
-                    col +=1
+                    col += 1
                 else:
                     valor += c
         return matriz, columnas
 
-def mostrar_mariz(matriz:list):
-    """Muestra la Matriz que se le pase 
+
+def mostrar_matriz(matriz: list):
+    """Muestra la Matriz que se le pase
 
     Args:
         matriz (_type_): Matriz a mostrar
     """
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
-            print(matriz[i][j], end= " ")
+            print(matriz[i][j], end=" ")
         print("")
 
-def filtrar_por_dificultad(matriz:list, columnas:list, dificultad:str)->list:
+
+def filtrar_por_dificultad(matriz: list, columnas: list, dificultad: str) -> list:
     """Muestra toda la fila de la dificultad que se le pase
 
     Args:
@@ -81,30 +87,32 @@ def filtrar_por_dificultad(matriz:list, columnas:list, dificultad:str)->list:
         if columnas[i] == "dificultad":
             indice_dificultad = i
             break
-    
-    filtradas =[]
+
+    filtradas = []
     for i in range(len(matriz)):
         if matriz[i][indice_dificultad] == dificultad:
             filtradas += [matriz[i]]
     return filtradas
 
-def filtrar_solo_preguntas(lista_ya_filtrada, columnas:list)->list:
-    inidice_preguntas =-1
+
+def filtrar_solo_preguntas(lista_ya_filtrada, columnas: list) -> list:
+    inidice_preguntas = -1
     for i in range(len(columnas)):
         if columnas[i] == "pregunta":
             inidice_preguntas = i
             break
-    preguntas =[]
+    preguntas = []
 
     for i in range(len(lista_ya_filtrada)):
         preguntas += [lista_ya_filtrada[i][inidice_preguntas]]
     return preguntas
-    
-matriz, columnas = llamar_csv()
-facil = filtrar_por_dificultad(matriz, columnas, "fácil" )
+
+
+# matriz, columnas = llamar_csv()
+# facil = filtrar_por_dificultad(matriz, columnas, "fácil")
 # mostrar_mariz(matriz)
 # for i in range(len(facil)):
 #     print(facil[i])
-preguntas = filtrar_solo_preguntas(facil, columnas)
-for i in range(len(preguntas)):
-    print(preguntas[i])
+# preguntas = filtrar_solo_preguntas(facil, columnas)
+# for i in range(len(preguntas)):
+#    print(preguntas[i])
--- main.py
+++ main.py
@@ -3,18 +3,27 @@ from os import system
 from funciones import *
 
 
-def main()->None:
-   print("BIENVENIDO A --SUPER-QUIZ-- ")
-   print("El juego favorito de tus SuperHeroe favorito!!")
-   preguntas = 7
-
-
-
-
-
-
-
-
+def main() -> None:
+    print("BIENVENIDO A --SUPER-QUIZ-- ")
+    print("El juego favorito de tus SuperHeroe favorito!!")
+    matriz, columnas = llamar_csv()
+    opciones = input(
+        "\ningrese la dificultad:" "\nFacil (1) " "\nNormal(2)" "\nDificil(3): "
+    )
+    preguntas = 7
+    match (opciones):
+        case "1":
+            preguntas_filtradas = filtrar_por_dificultad(matriz, columnas, "fácil")
+            for fila in preguntas_filtradas:
+                input(fila[0])
+        case "2":
+            preguntas_filtradas = filtrar_por_dificultad(matriz, columnas, "normal")
+            for fila in preguntas_filtradas:
+                input(fila[0])
+        case "3":
+            preguntas_filtradas = filtrar_por_dificultad(matriz, columnas, "difícil")
+            for fila in preguntas_filtradas:
+                print(fila[0])
 
 
 if __name__ == "__main__":
